a5b5a97a9e2a723584c7e049e6d2baf64f8ed140
OSZII <ostojicstefan2606@gmail.com>
2022-01-20 13:29:08 +0100

    Create file.ps1

---
+++ 20.01.2022/file.ps1
@@ -0,0 +1,20 @@
+# $PSVersionTable
+
+# Get-Alias
+
+# Get-Command
+
+# ipconfig.exe
+
+# Get-Module
+
+# Import-Module
+
+# Get-ChildItem
+
+# generiert ein 6 stellige zahlenreihenfolge die zufällig nummern von 1 bis 45 hat
+# 1..45 | Get-Random -Count 6
+
+# Get-ChildItem -Path C:\Users\Stefan\Desktop -Filter "*.bat" -Recurse -File | Select-Object -Property FullName, length | Sort-Object -Property length
+
+Write-Host "Hello World"
