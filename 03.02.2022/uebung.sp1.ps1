4e47a62be654c3f86f9f2bc7573765a1bc00ecbb
OSZII <ostojicstefan2606@gmail.com>
2022-02-03 13:07:27 +0100

    Create uebung.sp1.ps1

---
+++ 03.02.2022/uebung.sp1.ps1
@@ -0,0 +1,8 @@
+﻿# Erste Übung 1
+#Zeigen Sie den Namen der 5 größten PDF-Datein in Benutzerverzeichnis
+#Get-Childitem -Path $env:USERPROFILE -Filter "*.pdf" -Recurse -File | Sort-Object -Property Length -Descending | `
+#Select-Object -First 5 -Property FullName, Length
+
+#Erstelle 5 Testdateien im Benutzerordner: Test_1.txt, Test_2.txt
+#I bis 5
+(1..5) | ForEach-Object -Process { New-Item -ItemType File  -Path $env:USERPROFILE -Name Test_$_.txt }
