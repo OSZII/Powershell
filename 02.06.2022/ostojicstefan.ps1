c8dae80af29a1833c6437b863fb81ba7ed5cde25
OSZII <ostojicstefan2606@gmail.com>
2022-06-02 11:05:59 +0200

    c

---
+++ 02.06.2022/ostojicstefan.ps1
@@ -0,0 +1,9 @@
+# Aufgabenstellung
+# Skript erstellen, welches eine Funktion enthält namens checkPattern
+# die se funktion hat 3 parameter
+# einmal einen Pfad zu einem directory
+# ein dateityp .ps1, .txt, ...
+# suchstring
+# Als ausgabe alle zeilen aller gefundenen Dateien ausgegeben werden die den Suchstring enthalten
+# Außerdem eine Liste mit erstelldatum göße(length), author/uhrheber wenns den gibt, letzte änderung
+# Ausgabe wahlweise auf der Konsole(default) oder Textdatei(suchpfad) Dateinamen mit 00 anfangen lassen, damit es ganz oben ist
